594199e8d0e3b14b39eb783816f7e058294df193
Aditya Srivastav <137613253+AdityaSrivastav5@users.noreply.github.com>
2023-08-30 01:10:40 +0530

    Subset_2.cpp

---
+++ Subset_2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void subset(int ind,vector<int>&nums,vector<vector<int>>&ans,vector<int>&ds){
+        ans.push_back(ds);
+        for(int i=ind;i<nums.size();i++){
+            if(i!=ind && nums[i]==nums[i-1])continue;
+            ds.push_back(nums[i]);
+            subset(i+1,nums,ans,ds);
+            ds.pop_back();
+        }
+
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+      vector<vector<int>>ans;
+      vector<int>ds;
+      subset(0,nums,ans,ds);
+      return ans;  
+    }
+};
