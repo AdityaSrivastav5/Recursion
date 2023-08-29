8ea03ae9caa8556128e4f3a42cf27f985fadcfde
Aditya Srivastav <137613253+AdityaSrivastav5@users.noreply.github.com>
2023-08-30 02:32:13 +0530

    Permutations.cpp

---
+++ Permutations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    void solve(vector<int> nums,int i,vector<vector<int>>& ans){
+        if(i>=nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        for(int j=i;j<nums.size();j++){
+            swap(nums[i],nums[j]);
+            solve(nums,i+1,ans);
+            swap(nums[i],nums[j]);
+        }
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int i=0;
+        solve(nums,i,ans);
+        return ans;
+    }
+};
